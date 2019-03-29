913fb4822d30f1326edebf5d196f659c449d979a
Maul <mprambadi@gmail.com>
2019-03-29 14:53:36 +0700

    gh-pages

---
+++ precache-manifest.14b7918be0a7e72226148069450a4848.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af3f6dbe172a9c1aba77366a2ff630d1",
+    "url": "/todo-devsol/static/media/materialdesignicons-webfont.af3f6dbe.woff2"
+  },
+  {
+    "revision": "d2f011a74b828cbb4b48",
+    "url": "/todo-devsol/static/css/main.9f7261bd.chunk.css"
+  },
+  {
+    "revision": "88283e27b3ff1572d670",
+    "url": "/todo-devsol/static/js/runtime~main.006d10e6.js"
+  },
+  {
+    "revision": "032eb679a327d4668d7537c32c314fbc",
+    "url": "/todo-devsol/static/media/materialdesignicons-webfont.032eb679.svg"
+  },
+  {
+    "revision": "ebb8f9d46156c5c12a5a",
+    "url": "/todo-devsol/static/js/2.b1bc6b5d.chunk.js"
+  },
+  {
+    "revision": "d2f011a74b828cbb4b48",
+    "url": "/todo-devsol/static/js/main.47325944.chunk.js"
+  },
+  {
+    "revision": "26333b3884782fdb5f8c0a971f1d2570",
+    "url": "/todo-devsol/static/media/materialdesignicons-webfont.26333b38.woff"
+  },
+  {
+    "revision": "1993c4a16cce5446a5cfafacf4da740b",
+    "url": "/todo-devsol/static/media/materialdesignicons-webfont.1993c4a1.eot"
+  },
+  {
+    "revision": "baded94134f273450a473a4962111324",
+    "url": "/todo-devsol/static/media/materialdesignicons-webfont.baded941.ttf"
+  },
+  {
+    "revision": "ebb8f9d46156c5c12a5a",
+    "url": "/todo-devsol/static/css/2.f896daed.chunk.css"
+  },
+  {
+    "revision": "f27d2b58021313db98b5737b755ae78e",
+    "url": "/todo-devsol/index.html"
+  }
+];
